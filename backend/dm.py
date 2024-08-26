4f78e46f8f13ed4560a771684b01bd833634aeef
Zach <zachvehlert@gmail.com>
2024-08-26 15:21:04 -0600

    Backend stuff working properly, didn't commit it correctly so backend changes were not added

--- backend/dm.py
+++ backend/dm.py
@@ -12,9 +12,9 @@ In combat, make sure the player and adversary exchange blows.
 
 Never take an action for the player.
 Never provide a numbered or bulleted list of options to the player.
-"""}
-
-"""Check if this action, statement, question, or number in the "requested_action" field of the JSON is a 
+                        
+Check if this action, statement, question, or number in the "requested_action" field of the JSON is a 
 valid action. You must decide if the action is valid based on the information the player sends to you, 
 for example, if the player tries to use an item it must exist in the "usable_items" field, otherwise 
 the action is invalid."""
+}
--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,44 @@
 import os
+from flask import Flask, request
+from flask_cors import CORS
 from openai import OpenAI
 from dotenv import load_dotenv
 from dm import system_content
 from models import DMresponse
 
+# Load env variables
 load_dotenv()
+
+# Initialize openai stuff
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-completion = client.beta.chat.completions.parse(
-    model="gpt-4o-2024-08-06",
-    messages=[
-        system_content,
-        {"role": "user", "content": "I want to attack the dragon."},
-    ],
-    response_format=DMresponse,
-)
+# initialize flask app
+app = Flask(__name__)
+cors = CORS(app)
 
-event = completion.choices[0].message.parsed
-print(event)
+@app.route('/')
+def index():
+    return "Current routes: /get_response"
+
+@app.route('/get_response', methods=['GET', 'POST'])
+def get_response():
+    # Get the request data and jsonify it
+    request_data = request.get_json()
+
+    # OpenAI API call
+    completion = client.beta.chat.completions.parse(
+        model="gpt-4o-2024-08-06",
+        messages=[
+            system_content,
+            {"role": "user", "content": str(request_data)},
+        ],
+        response_format=DMresponse,
+    )
+
+    # Parse the response, turn it to json, return it
+    response = completion.choices[0].message.parsed
+    return response.model_dump()
+
+if __name__ == '__main__':
+    app.run(debug=True)
