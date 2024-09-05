471245dde2ed6c74d5b40c686d20e67c3aa81e92
Zach <zachvehlert@gmail.com>
2024-09-05 10:14:25 -0600

    Updated folder structure, App is now a page

--- frontend/chatrpg/src/main.jsx
+++ frontend/chatrpg/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './pages/App/App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import NewGame from './pages/NewGame/NewGame.jsx'
 import GameCreator from './pages/GameCreator/GameCreator.jsx'
